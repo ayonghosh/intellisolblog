0f0e8bc81f841159b68160a0cd4d668345a7b59d
Ayon Ghosh <ayonghosh88@gmail.com>
2013-10-18 21:32:55 +0530

    Intial commit

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require("express");
 
 var app = express();
 app.use(express.logger());
+app.use("/css", express.static('/css'));
+app.use("/js", express.static('/js'));
+app.use("/images", express.static('/images'));
 
 app.get('/', function(request, response) {
   response.sendfile('index.html');
