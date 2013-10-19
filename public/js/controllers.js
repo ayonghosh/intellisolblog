6dad90426f6e51d6e4fc2d8278cd9ac56d9e862d
Ayon Ghosh <ayonghosh88@gmail.com>
2013-10-19 10:33:08 +0530

    commit

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -10,7 +10,7 @@ intelliSolBlogControllers.controller('BlogListCtrl', ['$scope', '$http',
 				if (typeof data == 'string') {
 					data = JSON.parse(data);
 				}
-				// Because using jQuery ajax is out of the world of angular, we need to wrap our $scope assignment inside of:
+				// Because using jQuery ajax is out of the world of angular, we need to wrap our $scope assignment inside:
 				$scope.$apply(function(){
 				    $scope.blogs = data.response.posts;
 				});
@@ -20,7 +20,9 @@ intelliSolBlogControllers.controller('BlogListCtrl', ['$scope', '$http',
 ]);
 
 intelliSolBlogControllers.controller('BlogCtrl', ['$scope', '$routeParams', '$http', 
-	function($scope, $routeParams, $http) {
+	function BlogCtrl($scope, $routeParams, $http) {
+		console.log('hi');
+		console.log($routeParams.blogId);
 		$.ajax(
 		{
 			url:'http://api.tumblr.com/v2/blog/intellisol.tumblr.com/posts?api_key=T87SJzAER6jBO2qpIkDcvgqxMxTTZ3KClAQL04uJZJ0I878USq&id=' + $routeParams.blogId,
@@ -29,7 +31,9 @@ intelliSolBlogControllers.controller('BlogCtrl', ['$scope', '$routeParams', '$ht
 				if (typeof data == 'string') {
 					data = JSON.parse(data);
 				}
-				// Because using jQuery ajax is out of the world of angular, we need to wrap our $scope assignment inside of:
+				
+				console.log(data);
+				// Because using jQuery ajax is out of the world of angular, we need to wrap our $scope assignment inside:
 				$scope.$apply(function(){
 					$scope.title = data.response.posts[0].title;
 				    $scope.text = data.response.posts[0].body;
