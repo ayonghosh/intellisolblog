01a3d2d7e204a01fc3eccd9ffb87b53bd0c9a5a3
Ayon Ghosh <ayonghosh88@gmail.com>
2013-10-19 09:20:04 +0530

    commit

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,6 +2,7 @@ var intelliSolBlogControllers = angular.module('blogControllers', []);
 
 intelliSolBlogControllers.controller('BlogListCtrl', ['$scope', '$http', 
 	function BlogListCtrl($scope, $http) {
+		var _scope = $scope;
 		$.ajax(
 		{
 			url:'http://api.tumblr.com/v2/blog/intellisol.tumblr.com/posts?api_key=T87SJzAER6jBO2qpIkDcvgqxMxTTZ3KClAQL04uJZJ0I878USq',
@@ -10,16 +11,8 @@ intelliSolBlogControllers.controller('BlogListCtrl', ['$scope', '$http',
 				if (typeof data == 'string') {
 					data = JSON.parse(data);
 				}
-				//$scope.blogs = data.response.posts;
-				$scope.blogs = [
-				    {'title': 'Nexus S',
-				     'snippet': 'Fast just got faster with Nexus S.'},
-				    {'title': 'Motorola XOOM™ with Wi-Fi',
-				     'snippet': 'The Next, Next Generation tablet.'},
-				    {'title': 'MOTOROLA XOOM™',
-				     'snippet': 'The Next, Next Generation tablet.'}
-				  ];
-				console.log($scope.blogs);
+				_scope.blogs = data.response.posts;
+				console.log(_scope.blogs);
 			}
 		});
 	}
