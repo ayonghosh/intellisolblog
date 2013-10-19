e4a9a67e031c9a7d4a687f0b98ad49c5ba2f046d
Ayon Ghosh <ayonghosh88@gmail.com>
2013-10-19 08:46:21 +0530

    commit

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ var express = require("express");
 
 var app = express();
 app.use(express.logger());
-app.use("/css", express.static('public/css'));
-app.use("/js", express.static('public/js'));
-app.use("/images", express.static('public/images'));
+app.use('/css', express.static('public/css'));
+app.use('/js', express.static('public/js'));
+app.use('/images', express.static('public/images'));
+app.use('/', express.static('public/'));
 
 app.get('/', function(request, response) {
   response.sendfile('public/index.html');
