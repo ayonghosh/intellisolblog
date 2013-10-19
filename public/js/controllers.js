77a6fd98fb1cfabd88d985b72705bc3620c8ff12
Ayon Ghosh <ayonghosh88@gmail.com>
2013-10-19 10:11:10 +0530

    commit

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,7 +2,7 @@ var intelliSolBlogControllers = angular.module('blogControllers', []);
 
 intelliSolBlogControllers.controller('BlogListCtrl', ['$scope', '$http', 
 	function BlogListCtrl($scope, $http) {
-		/*$.ajax(
+		$.ajax(
 		{
 			url:'http://api.tumblr.com/v2/blog/intellisol.tumblr.com/posts?api_key=T87SJzAER6jBO2qpIkDcvgqxMxTTZ3KClAQL04uJZJ0I878USq',
 			dataType: 'jsonp',
@@ -10,14 +10,17 @@ intelliSolBlogControllers.controller('BlogListCtrl', ['$scope', '$http',
 				if (typeof data == 'string') {
 					data = JSON.parse(data);
 				}
-				$scope.blogs = data.response.posts;
-				console.log($scope.blogs);
+				$scope.$apply(function(){
+				    $scope.blogs = data.response.posts;
+				});
+				//$scope.blogs = data.response.posts;
+				//console.log($scope.blogs);
 			}
-		});*/
-		
+		});
+		/*
 		$http.get('http://api.tumblr.com/v2/blog/intellisol.tumblr.com/posts?api_key=T87SJzAER6jBO2qpIkDcvgqxMxTTZ3KClAQL04uJZJ0I878USq&?callback=JSON_CALLBACK').success(function(data) {
 			$scope.blogs = data.response.posts;
-		});
+		});*/
 	}
 ]);
 
