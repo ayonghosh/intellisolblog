03b8eed78461ff758257d551580367c599e80053
Ayon Ghosh <ayonghosh88@gmail.com>
2013-10-19 10:25:31 +0530

    commit

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -10,22 +10,31 @@ intelliSolBlogControllers.controller('BlogListCtrl', ['$scope', '$http',
 				if (typeof data == 'string') {
 					data = JSON.parse(data);
 				}
+				// Because using jQuery ajax is out of the world of angular, we need to wrap our $scope assignment inside of:
 				$scope.$apply(function(){
 				    $scope.blogs = data.response.posts;
 				});
-				//$scope.blogs = data.response.posts;
-				//console.log($scope.blogs);
 			}
 		});
-		/*
-		$http.get('http://api.tumblr.com/v2/blog/intellisol.tumblr.com/posts?api_key=T87SJzAER6jBO2qpIkDcvgqxMxTTZ3KClAQL04uJZJ0I878USq&?callback=JSON_CALLBACK').success(function(data) {
-			$scope.blogs = data.response.posts;
-		});*/
 	}
 ]);
 
-intelliSolBlogControllers.controller('BlogCtrl', ['$scope', '$routeParams', 
-	function($scope, $routeParams) {
-		// TODO 
+intelliSolBlogControllers.controller('BlogCtrl', ['$scope', '$routeParams', '$http'
+	function($scope, $routeParams, $http) {
+		$.ajax(
+		{
+			url:'http://api.tumblr.com/v2/blog/intellisol.tumblr.com/posts?api_key=T87SJzAER6jBO2qpIkDcvgqxMxTTZ3KClAQL04uJZJ0I878USq&id=' + $routeParams.blogId,
+			dataType: 'jsonp',
+			success: function(data) {
+				if (typeof data == 'string') {
+					data = JSON.parse(data);
+				}
+				// Because using jQuery ajax is out of the world of angular, we need to wrap our $scope assignment inside of:
+				$scope.$apply(function(){
+					$scope.title = data.response.posts[0].title;
+				    $scope.text = data.response.posts[0].body;
+				});
+			}
+		});
 	}
 ])
