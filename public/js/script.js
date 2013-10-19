4a79c714672193fef2c6dab52aba41617cf683c0
Ayon Ghosh <ayonghosh88@gmail.com>
2013-10-19 07:58:22 +0530

    commit

--- public/js/script.js
+++ public/js/script.js
@@ -1,23 +1,36 @@
 
 // application namespace
-var app = app || {};
+var blogApp = blogApp || {};
 
 // all blog posts
-app.posts = [];
+blogApp.posts = [];
 
 // function to retrieve all blog posts
-app.getPosts = function() {
-	$.get("http://api.tumblr.com/v2/blog/intellisol.tumblr.com/posts?api_key=T87SJzAER6jBO2qpIkDcvgqxMxTTZ3KClAQL04uJZJ0I878USq", 
-	function(data) {
-		if (typeof data == 'string') {
-			app.posts = JSON.parse(data);
-		}else {
-			app.posts = data;
+blogApp.getPosts = function() {
+	var _this = this;
+	$.ajax(
+	{
+		url:"http://api.tumblr.com/v2/blog/intellisol.tumblr.com/posts?api_key=T87SJzAER6jBO2qpIkDcvgqxMxTTZ3KClAQL04uJZJ0I878USq",
+		dataType: "jsonp",
+		success: function(data) {
+			if (typeof data == "string") {
+				_this.posts = JSON.parse(data);
+			}else {
+				_this.posts = data;
+			}
+			console.log(_this.posts.response.posts);
+			_this.showPostIndex();
 		}
-		console.log(posts);
 	});
 }
 
+blogApp.showPostIndex = function() {
+	var html = "";
+	for (var i = 0; i < this.posts.response.posts.length; i++) {
+		html += this.posts.response.posts[i].title + "<br>";
+	}
+	$("#content").html(html);
+}
 
 // get all blog posts
-app.getPosts();
+blogApp.getPosts();
